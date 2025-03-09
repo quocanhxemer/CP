358ce50188612bc0b86e158799695411b4d0fffe
Anh Pham <quocanhxemer@gmail.com>
2025-03-10 00:12:18 +0100

    first commit

---
+++ graphs/bfs/leetcode/2583.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    long long kthLargestLevelSum(TreeNode *root, int k) {
+        long long sum[100000];
+        memset(sum, 0, sizeof(sum));
+
+        queue<TreeNode *> c;
+        c.push(root);
+
+        int current_level = 0;
+        while (!c.empty()) {
+            queue<TreeNode *> next;
+
+            while (!c.empty()) {
+                TreeNode *current = c.front();
+                c.pop();
+                sum[current_level] += current->val;
+
+                if (current->left) {
+                    next.push(current->left);
+                }
+                if (current->right) {
+                    next.push(current->right);
+                }
+            }
+
+            c = next;
+            current_level++;
+        }
+
+        if (k > current_level) {
+            return -1;
+        }
+
+        sort(sum, sum + current_level);
+        reverse(sum, sum + current_level);
+
+        return sum[k - 1];
+    }
+};
