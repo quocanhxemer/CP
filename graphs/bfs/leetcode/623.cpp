c4d27b14a73b3cac7d25b77ce402f225cd96ecd5
kyredyrf nipohc <quocanhxemer@gmail.com>
2025-03-11 16:58:49 +0100

    623 - Add one row to tree - functional programming way

---
+++ graphs/bfs/leetcode/623.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        return add(root, val, depth, true);
+    }
+
+   private:
+    TreeNode* add(TreeNode* root, int val, int depth, bool isLeft) {
+        if (depth == 1) {
+            if (isLeft) {
+                return new TreeNode(val, root, nullptr);
+            } else {
+                return new TreeNode(val, nullptr, root);
+            }
+        }
+
+        if (!root) {
+            return nullptr;
+        }
+
+        return new TreeNode(root->val, add(root->left, val, depth - 1, true),
+                            add(root->right, val, depth - 1, false));
+    }
+};
